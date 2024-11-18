545c6271466dcf116f4388298e97e089139adad6
Agustin Varas <Agustin.Varas@outlook.com.ar>
2024-11-18 13:40:08 +0000

    Creado el menu principal del script salon.sh

--- salon.sh
+++ salon.sh
@@ -1,9 +1,20 @@
 #! /bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=salon -c"
+PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
 
 MAIN_MENU() {
   echo -e "\n~~~~ MY SALON ~~~~\n"
+  SERVICES_SELECT=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id" )
+  if [[ -z $SERVICES_SELECT ]]
+  then
+    return 'no services available'
+  else
+    echo "Welcome, how can we help you?"
+    echo "$SERVICES_SELECT" | while read SERVICE_ID BAR NAME
+    do          
+      echo "$SERVICE_ID) $NAME" 
+    done
+  fi
 }
 
 MAIN_MENU
