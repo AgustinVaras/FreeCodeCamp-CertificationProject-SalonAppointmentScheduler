5b3ba987550f5edc3fe2ce72d3e49aa1bd7e28c6
Agustin Varas <Agustin.Varas@outlook.com.ar>
2024-11-19 15:42:40 +0000

    Refactorizado del código para que el menu principal maneje el llamado a funciones

--- salon.sh
+++ salon.sh
@@ -2,23 +2,12 @@
 
 PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
 
-MAIN_MENU() {
-  echo -e "\n~~~~ MY SALON ~~~~\n"
+MAIN_MENU() { 
   if [[ $1 ]]
   then
     echo -e "\n$1"
-    SERVICES_MENU
-  else
-    SERVICES_MENU
   fi
-}
-
-EXIT_MENU() {
-  echo -e "\nThank you for coming"
-  exit
-}
-
-SERVICES_MENU() {
+  echo -e "\n~~~~ MY SALON ~~~~\n"
   SERVICES_SELECT=$($PSQL "SELECT service_id, name FROM services" )
   if [[ -z $SERVICES_SELECT ]]
   then
@@ -32,18 +21,15 @@ SERVICES_MENU() {
     echo "---------"
     echo "0) Exit"
     read SERVICE_ID_TO_SCHEDULE
-
     #Exit Program
     if [[ $SERVICE_ID_TO_SCHEDULE == 0 ]]
     then
-      EXIT_MENU
+      EXIT
     fi
-
     if [[ ! $SERVICE_ID_TO_SCHEDULE =~ ^[0-9]+$ ]]
     then
       MAIN_MENU 'That is not a valid service number'
     else
-
       #Validate existint service
       VALID_SERVICE=false
       echo "$SERVICES_SELECT" | while read SERVICE_ID BAR NAME
@@ -53,7 +39,6 @@ SERVICES_MENU() {
           VALID_SERVICE=true          
         fi
       done
-
       if [[ $VALID_SERVICE != true ]] 
       then
         MAIN_MENU "That's not a valid service number"
@@ -61,19 +46,27 @@ SERVICES_MENU() {
         #Read phone number
         echo -e "\nPlease input your phone number"
         read PHONE_NUMBER
-
         #Get customer ID and Name
         echo "$($PSQL "SELECT customer_id, name FROM customers WHERE phone = '$PHONE_NUMBER'" )" | read CUSTOMER_ID BAR NUMBER 
-
         #If not found
         if [[ -z $CUSTOMER_ID ]]
         then
           echo -e "\nI couldn't find that phone number, what's your name?"
+          read NAME
+          #Insert new customer
         fi
-
       fi
     fi
   fi
-} 
+}
 
+
+ 
+
+EXIT() { #Función de salida del programa
+  echo -e "\nThank you for coming"
+  exit
+}
+
+#Ejecución
 MAIN_MENU
